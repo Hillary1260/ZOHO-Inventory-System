b4ed41f375aea01343ded37728babd39cd4ad846
Hillary1260 <67297200+Hillary1260@users.noreply.github.com>
2020-06-23 02:04:21 +0200

    Initial commit

---
+++ ZOHO Inventory System/src/dunnsform/DunnsForm.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dunnsform;
+
+/**
+ *
+ * @author Hillary Kahari
+ */
+public class DunnsForm {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    WelcomePage welcomePage = new WelcomePage();
+    welcomePage.show();
+    welcomePage.setSize(1400,700);
+    welcomePage.setResizable(false);
+    } 
+}
